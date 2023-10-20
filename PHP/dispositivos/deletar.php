92428f089343418f69384f412eb336b23a03d4fd
Victor Lopes <99414020+victor-lopps@users.noreply.github.com>
2023-10-19 21:26:08 -0300

    Update deletar.php

--- PHP/dispositivos/deletar.php
+++ PHP/dispositivos/deletar.php
@@ -1,12 +1,13 @@
 <?php
 require_once './conexao.php';
+session_start();
 
 $success_message = '';
 $error_message = '';
 
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-   
+if ($_SERVER['REQUEST_METHOD'] === 'POST') { 
+        $userid = $_SESSION['user_id'];
         $dispid = $_POST['id'];
         $name= $_POST['nome'];
 
@@ -14,12 +15,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $error_message = "Digite digite o nome e id do dispositivo.";
         } else {
             // Comando SQL para verificar se o usuário e a senha correspondem aos dados do banco
-            $sql_verify = "SELECT * FROM ifound WHERE id = '$dispid' AND nome = '$name'";
+            $sql_verify = "SELECT * FROM ifound WHERE id = '$dispid' AND nome = '$name' AND user_id = $userid";
             $result = $conn->query($sql_verify);
 
             if ($result->num_rows > 0) {
                 // Usuário encontrado com a senha correta, então podemos proceder com a exclusão
-                $sql_delete = "DELETE FROM ifound WHERE id = '$dispid' AND nome = '$name'";
+                $sql_delete = "DELETE FROM ifound WHERE id = '$dispid' AND nome = '$name' AND user_id = $userid";
                 if ($conn->query($sql_delete) === TRUE) {
                   echo "<script type= 'text/javascript'>alert('Exclusão bem sucedida!');";
                   echo "javascript:window.location='../../HTML/dispositivos/delete.php';</script>";
