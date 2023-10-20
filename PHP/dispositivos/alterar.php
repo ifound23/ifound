b9a8ac9f4aeb2366a8023d2a23d4dd67790598c2
Victor Lopes <99414020+victor-lopps@users.noreply.github.com>
2023-10-19 21:25:36 -0300

    Update alterar.php

--- PHP/dispositivos/alterar.php
+++ PHP/dispositivos/alterar.php
@@ -1,8 +1,10 @@
 <?php
 
 require_once './conexao.php';
+session_start();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $userid = $_SESSION['user_id'];
   $dispid = $_POST['id'];
   $name = $_POST['nome'];
   $newname = $_POST['newnome'];
@@ -11,11 +13,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     echo "<script type='text/javascript'>alert('Digite seu usuário e senha.');";
     echo "window.location='../html/alterar.html';</script>";
   } else {
-    $sql_verify = "SELECT * FROM ifound WHERE id = '$dispid' AND nome = '$name'";
+    $sql_verify = "SELECT * FROM ifound WHERE id = '$dispid' AND nome = '$name' AND user_id = '$userid'";
     $result = $conn->query($sql_verify);
 
     if ($result->num_rows > 0) {
-      $sql_update = "UPDATE ifound SET nome = '$newname' WHERE nome = '$name' AND id = '$dispid'";
+      $sql_update = "UPDATE ifound SET nome = '$newname' WHERE nome = '$name' AND id = '$dispid' AND user_id = '$userid'";
       if ($conn->query($sql_update) === TRUE) {
         echo "<script type='text/javascript'>alert('Alteração bem sucedida!');";
         echo "window.location='../../HTML/dispositivos/alterar.php';</script>";
