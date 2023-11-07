c06d6d3505e43af080a82bfb296e70680df7b1fa
Victor Lopes <99414020+victor-lopps@users.noreply.github.com>
2023-11-07 20:13:37 -0300

    Update dispositivos.sql

--- DB/dispositivos.sql
+++ DB/dispositivos.sql
@@ -2,6 +2,17 @@ CREATE DATABASE IF NOT EXISTS ifound;
 
 -- Use o banco de dados ifound
 USE ifound;
+
+-- Crie a tabela user para armazenar informações de usuários
+CREATE TABLE user (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(250),
+    nome VARCHAR(250),
+    senha VARCHAR(50),
+    status_conta VARCHAR(15)
+);
+
+-- Crie a tabela dispositivo para armazenar informações de dispositivos e relacione com a tabela user usando a coluna user_id
 CREATE TABLE ifound (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(50) NOT NULL,
